b4ba443759fbec828063bad0bf07265ba4a6ca4c
hiroshi <hiroshi3110@gmail.com>
2016-11-22 21:38:38 +0900

    yarn add primus.

---
+++ client.js
@@ -0,0 +1,11 @@
+// const Primus = require('./primus-client')
+// const primus = new Primus()
+// primus.
+const Primus = require('primus')
+const socket = new Primus.createSocket()
+const client = socket('http://localhost:8080')
+client.write({put: {tags: ['random'], message: 'hello'}})
+
+// client.on('data', data => {
+//   console.log('data:', data)
+// })
--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const pg = require('pg')
-const client = new pg.Client({user: 'postgres', database: 'postgres'})
-client.connect(err => {
-  if (err) throw err
-  client.query('SELECT 1', (err, result) => {
-    console.log(result.rows[0])
-    client.end(err => {
-      if (err) throw err
-    })
+// const server = require('http').createServer((req, res) => {
+// })
+
+const primus = require('primus').createServer(spark => {
+  spark.on('data', data => {
+    console.log(spark.id, ': data:', data)
+    // spark.write(data)
+    if (data.put) {
+      console.log('TODO insert item:', data.put)
+    }
   })
-})
+  spark.on('end', () => {
+    console.log(spark.id, ': end.')
+  })
+}, {port:8080, transformer: 'websockets'})
+primus.save(__dirname + '/primus-client.js')
+// server.listen(8080, () => {
+//   console.log('listen port 8080')
+// })
+
+
+
+
+
+// process.exit(0)
+
+// const pg = require('pg')
+// const client = new pg.Client({user: 'postgres', database: 'postgres'})
+// client.connect(err => {
+//   if (err) throw err
+//   client.query('SELECT 1', (err, result) => {
+//     console.log(result.rows[0])
+//     client.end(err => {
+//       if (err) throw err
+//     })
+//   })
+// })
